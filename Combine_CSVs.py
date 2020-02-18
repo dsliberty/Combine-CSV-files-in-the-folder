77a327525f9b513422ade5d99ca2949294aabb19
Dave S <ds.liberty@gmail.com>
2020-02-18 11:37:38 -0700

    Update Combine_CSVs.py

--- Combine_CSVs.py
+++ Combine_CSVs.py
@@ -1,26 +1,25 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Jan 12 06:56:16 2019
-
-@author: Chris
+@dsliberty edited to PEP 8 on 2020-02-18
+Credits:
+Author: @ekapope
+origin: @mintgreen A: @thclpr
+https://stackoverflow.com/q/9234560/1406733
 """
-#credited:
-#https://stackoverflow.com/questions/9234560/find-all-csv-files-in-a-directory-using-python/12280052
 
 import os
 import glob
 import pandas as pd
-#set working directory
+# set working directory
 os.chdir("/mydir")
 
-#find all csv files in the folder
-#use glob pattern matching -> extension = 'csv'
-#save result in list -> all_filenames
+# find all csv files in the folder
+# use glob pattern matching -> extension = 'csv'
+# save result in list -> all_filenames
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-#print(all_filenames)
+# print(all_filenames)
 
-#combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+# export to csv
+combined_csv.to_csv("combined_csv.csv", index=False, encoding='utf-8-sig')
